8369ffbb969e1285d3f227adbd346502ccf816ed
lafitewu <863512936@qq.com>
2020-06-03 17:03:58 +0800

    nuxt页面初始化

---
+++ pages/demo.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+ .container 
+  | 6666
+  span dsdsds
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  span {
+    color: red
+  }
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+ <span>55</span>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+</style>
